307e40baa44583e771403784cf53d4296c5d97e2
Lorraineken <lorrainekupa@gmail.com>
2023-01-14 17:50:00 +0300

    Displayed each task

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({taskData}) {
+
+const displayTasks=taskData.map((data) => {
+  return (
+    <Task key={data.id} category={data.category} text={data.text}/>
+  )
+})
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {displayTasks}
     </div>
   );
 }
